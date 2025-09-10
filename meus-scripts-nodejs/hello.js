b20555f02f4d3d310fa63fca5b0906fda4b04764
VitorDev1012 <vitormiyamoto.1bmangal@gmail.com>
2025-09-10 13:43:43 -0300

    Add files via upload

---
+++ meus-scripts-nodejs/hello.js
@@ -0,0 +1,4 @@
+console. log("Olá, mundo! Este é meu primeiro script Node.js!");
+console. log("Parabens! Voce esta programando!");
+console.log("Node.js versão:", process.version);
+console. log("Sistema operacional:", process.platform);
